1b60d7fff8ca6cfd6ef257451a3da1ff00ca2512
teoalexandrescu <teodora.alexander@gmail.com>
2024-09-15 13:13:34 -0500

    add new variable c

--- avg_integer.java
+++ avg_integer.java
@@ -4,8 +4,9 @@ class Main{
 
         int a = 5;
         int b = 18;
+        int c = 2;
 
-    int avg = (a + b)/2;
+    int avg = (a + b + c)/3;
     System.out.println("The average is:" + avg);
 
 }}
